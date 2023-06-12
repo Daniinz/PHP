735fd53292692000819af68519a122a8e55abeaa
Daniinz <136202226+Daniinz@users.noreply.github.com>
2023-06-11 22:23:41 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = "db4free.net:3306";
-$usuario = "danilo123";
-$senha = "123321da";
-$dbname = "bancotrab";
-// conexao
-$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
-?>
